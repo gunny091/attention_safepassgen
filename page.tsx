f8a932b136d82a2c175d6d24866e021fb09ece5d
chadol27 <ryuee091119@naver.com>
2025-06-08 02:16:37 +0900

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import SafePasswordGen from "./SafePasswordGen";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Attention Project",
@@ -14,6 +15,11 @@ export default function Page() {
         <p>안전한 비밀번호 생성기</p>
       </hgroup>
       <p>이 프로그램은 와부고등학교 동아리 ATTENTION의 프로젝트로 제작되었습니다.</p>
+      <p>
+        <Link href="https://github.com/gunny091/attention_safepassgen" target="_blank">
+          코드 보기
+        </Link>
+      </p>
       <SafePasswordGen />
     </>
   );
